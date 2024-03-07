5fd398bb861e2c0f7bac807eb8daac94f6bcadf3
Shenandoah Bennett <github.com@shenanb.com>
2024-03-06 20:58:14 -0800

    Populated index.php at the root for testing

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Quick Intro to PHP</title>
+</head>
+<body>
+    <h1>Turtle Quotes API</h1>
+    <?php echo "This is PHP code\n"; ?>
+    <p>The API can be accessed at /api</p>
+</body>
+</html>
