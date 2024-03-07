e8de8f00501a06a7261e0396f8c2b53ea60b2731
Shenandoah Bennett <github.com@shenanb.com>
2024-03-06 18:38:34 -0800

    Updated database.php for naming consistency

--- config/Database.php
+++ config/Database.php
@@ -4,11 +4,11 @@
         private $host;
         private $port;
         private $db_name;
-        private $username;
+        private $user;
         private $password;
 
         public function __construct() {
-            $this->username = getenv('USER');
+            $this->user = getenv('USER');
             $this->password = getenv('PASSWORD');
             $this->dbname = getenv('DBNAME');
             $this->host = getenv('HOST');
@@ -22,7 +22,7 @@
                 $dsn = "pgsql:host={$this->host};port={$this->port};dbname={$this->dbname}";
 
                 try {
-                    $this->conn = new PDO($dsn, $this->username, $this->password);
+                    $this->conn = new PDO($dsn, $this->user, $this->password);
                     $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                     return $this->conn;
                 } catch(PDOException $e) {
