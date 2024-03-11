85f26b946b97b25ec90c77c1b8a299cbc5b1b8b9
Shenandoah Bennett <github.com@shenanb.com>
2024-03-10 18:23:49 -0700

    Fixed categories update.

--- api/categories/update.php
+++ api/categories/update.php
@@ -6,42 +6,42 @@
     header('Access-Control-Allow-Headers: Access-Control-Allow-Headers, Content-Type, Access-Control-Allow-Methods, Authorization, X-Requested-With');
 
     include_once '../../config/Database.php';
-    include_once '../../models/Author.php';
+    include_once '../../models/Category.php';
 
     // Instantiate DB & connect
     $database = new Database();
     $db = $database->connect();
 
-    if(!isset($data) || !isset($data->id) || !isset($data->author)) {
+    if(!isset($data) || !isset($data->id) || !isset($data->category)) {
         echo json_encode(
             array('message' => 'Missing Required Parameters')
         );
     } else {
-        // Instantiate Author object
-        $author = new Author($db);
+        // Instantiate Category object
+        $category = new Category($db);
 
         // Set id for update
-        $author->id = $data->id;
+        $category->id = $data->id;
 
-        // Check if author_id exists
-        $author->read_single();
-        if(isset($author->author)) {
-            // Set author for update
-            $author->author = $data->author;
-            // Update author
-            $author->update();
+        // Check if category_id exists
+        $category->read_single();
+        if(isset($category->category)) {
+            // Set category for update
+            $category->category = $data->category;
+            // Update category
+            $category->update();
             // Read updated record and prepare array
-            $author->read_single();
-            $author_arr = array(
-                'id' => $author->id,
-                'author' => $author->author,
+            $category->read_single();
+            $category_arr = array(
+                'id' => $category->id,
+                'category' => $category->category,
             );
             // Make JSON
-            print_r(json_encode($author_arr));
+            print_r(json_encode($category_arr));
         } else {
-            // Return message from read_single() that author_id is not found
+            // Return message from read_single() that category_id is not found
             echo json_encode(
-                array('message' => 'author_id Not Found')
+                array('message' => 'category_id Not Found')
             );
         }
 
